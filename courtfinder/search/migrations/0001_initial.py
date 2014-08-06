1e1c02edd9c6c98a12abb66b797e83bad3af899d
Mehmet Duran <mehmet.duran@gmail.com>
2014-08-06 16:43:44 +0100

    Upgraded to Django 1.7

---
+++ courtfinder/search/migrations/0001_initial.py
@@ -0,0 +1,153 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='AddressType',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=255)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Area',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=255)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='AreaOfLaw',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=255)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Country',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=255, choices=[(b'ENG', b'England'), (b'NIR', b'Northern Ireland'), (b'SCT', b'Scotland'), (b'WLS', b'Wales')])),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='County',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=255)),
+                ('country', models.ForeignKey(to='search.Country')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Court',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=255)),
+                ('slug', models.SlugField(max_length=255)),
+                ('displayed', models.BooleanField(default=False)),
+                ('lat', models.IntegerField()),
+                ('lon', models.IntegerField()),
+                ('area', models.ForeignKey(to='search.Area')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='CourtAddress',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('address', models.TextField()),
+                ('postcode', models.CharField(max_length=255)),
+                ('address_type', models.ForeignKey(to='search.AddressType')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='court',
+            name='addresses',
+            field=models.ManyToManyField(to='search.AddressType', through='search.CourtAddress'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='courtaddress',
+            name='court',
+            field=models.ForeignKey(to='search.Court'),
+            preserve_default=True,
+        ),
+        migrations.CreateModel(
+            name='CourtAttribute',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('value', models.TextField()),
+                ('court', models.ForeignKey(to='search.Court')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='CourtAttributeType',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=255)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='courtattribute',
+            name='attribute_type',
+            field=models.ForeignKey(to='search.CourtAttributeType'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='court',
+            name='attributes',
+            field=models.ManyToManyField(to='search.CourtAttributeType', through='search.CourtAttribute'),
+            preserve_default=True,
+        ),
+        migrations.CreateModel(
+            name='Town',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=255)),
+                ('county', models.ForeignKey(to='search.County')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='courtaddress',
+            name='town',
+            field=models.ForeignKey(to='search.Town'),
+            preserve_default=True,
+        ),
+    ]
