f3f2dbedcdfdabf1f8c581f8b54c372337864c26
Jairo Diaz <jairo.diaz@codescrum.com>
2014-08-18 17:22:51 +0100

    Added test for checking for court data present in the subscriber call

--- courtfinder/subscriber/tests.py
+++ courtfinder/subscriber/tests.py
@@ -14,13 +14,18 @@ class SubscriberTestCase(unittest.TestCase):
 
     # def tearDown(self):
 
-    def test_post_on_url_endpoint(self):
+    def test_post_on_url_endpoint_without_data(self):
+      # 400 Bad Request - Often missing a required parameter.
       response = self.client.post('/subscriber/update')
-      self.assertEqual(response.status_code, 200)
+      self.assertEqual(response.status_code, 400)
 
     def test_get_on_url_endpoint(self):
       response = self.client.get('/subscriber/update')
       self.assertEqual(response.status_code, 405)
+
+    def test_post_on_url_endpoint_with_data(self):
+      response = self.client.post('/subscriber/update', { "court": '{name": "Abergavenny Magistrates Court"}' })
+      self.assertEqual(response.status_code, 200)
 
 if __name__ == '__main__':
     unittest.main()
--- courtfinder/subscriber/views.py
+++ courtfinder/subscriber/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseBadRequest
 from django.views.decorators.http import require_POST
 
 # Create your views here.
 
 @require_POST
 def update(request):
-  return HttpResponse('<p></p>')
+  if request.POST == {}:
+    return HttpResponseBadRequest('{"error": "missing area of law"}')
+  return HttpResponse('{"success"}')
