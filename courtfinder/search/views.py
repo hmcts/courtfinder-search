2a7d4c9fe5926f66199ecddc8226189d6031f911
Max Froumentin <max.froumentin@digital.justice.gov.uk>
2014-08-27 11:27:29 +0100

    factor

--- courtfinder/search/views.py
+++ courtfinder/search/views.py
@@ -28,6 +28,19 @@ def search_by_postcode(request):
 def search_by_address(request):
   return render(request, 'search/address.jinja')
 
+def format_results(results):
+  """
+  create a list of courts from search results that we can send to templates
+  """
+  courts=[]
+  for result in results:
+    addresses=[]
+    for address in result.courtaddress_set.all():
+      addresses.append({'type':address.address_type,
+                        'address':address.address.split('\n')})
+    court = { 'name': result.name, 'addresses': addresses }
+    courts.append(court)
+  return courts
 
 def results(request):
   if 'q' in request.GET:
@@ -36,19 +49,10 @@ def results(request):
     c = CourtSearch()
     results = c.address_search(query)
 
-    courts=[]
-    for result in results:
-      addresses=[]
-      for address in result.courtaddress_set.all():
-        addresses.append({'type':address.address_type, 'address':address.address.split('\n')})
-      court = { 'name': result.name, 'addresses': addresses }
-      courts.append(court)
-
     return render(request, 'search/results.jinja', {
       'query': query,
-      'search_results': courts
+      'search_results': format_results(results)
     })
-
   else:
     postcode = request.GET['postcode']
     area_of_law = request.GET['area_of_law']
@@ -59,18 +63,9 @@ def results(request):
     c = CourtSearch()
     results = c.postcode_search(postcode, area_of_law)
 
-    courts=[]
-    for result in results:
-      addresses=[]
-      for address in result.courtaddress_set.all():
-        addresses.append({'type':address.address_type, 'address':address.address.split('\n')})
-      court = { 'name': result.name, 'addresses': addresses }
-      courts.append(court)
-
-
     return render(request, 'search/results.jinja', {
       'postcode': postcode,
       'area_of_law': area_of_law,
       'areas_of_law': AreaOfLaw.objects.all(),
-      'search_results': courts
+      'search_results': format_results(results)
     })
