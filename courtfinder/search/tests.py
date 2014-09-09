e2e77bd9289171d3f70d033440ed7485f430e9e5
Max Froumentin <max.froumentin@digital.justice.gov.uk>
2014-09-09 15:58:32 +0100

    more tests and use coverage

--- courtfinder/search/tests.py
+++ courtfinder/search/tests.py
@@ -5,7 +5,6 @@ class SearchTestCase(TestCase):
     def test_top_page_sans_slash_redirects_to_slash(self):
         c = Client()
         response = c.get('/search')
-        print response
         self.assertRedirects(response, '/search/', 301)
 
     def test_top_page_returns_correct_content(self):
@@ -14,3 +13,23 @@ class SearchTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'search/index.jinja')
         self.assertInHTML('<title>Find a court or tribunal</title>', response.content, count=1)
+
+    def test_sample_postcode_all_aols(self):
+        c = Client()
+        response = c.get('/search/results?postcode=SE15+4UH&area_of_law=All')
+        self.assertEqual(response.status_code, 200)
+
+    def test_sample_postcode_specific_aol(self):
+        c = Client()
+        response = c.get('/search/results?postcode=SE15+4UH&area_of_law=Divorce')
+        self.assertEqual(response.status_code, 200)
+
+    def test_partial_postcode(self):
+        c = Client()
+        response = c.get('/search/results?postcode=SE15&area_of_law=All')
+        self.assertEqual(response.status_code, 200)
+
+    def test_address_search(self):
+        c = Client()
+        response = c.get('/search/results?q=Leeds')
+        self.assertEqual(response.status_code, 200)
