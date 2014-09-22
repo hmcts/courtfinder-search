e3b7928f85680fbacd4857b1e1f054599c9cd840
Max Froumentin <max.froumentin@digital.justice.gov.uk>
2014-09-22 12:09:13 +0100

    partial commit

--- courtfinder/update/tests.py
+++ courtfinder/update/tests.py
@@ -3,17 +3,12 @@ import requests
 from mock import Mock, patch
 from search.models import *
 from django.conf import settings
-
+from update import court_import
 
 class SearchTestCase(TestCase):
 
     def setUp(self):
-        self.court = Court.objects.create(
-            admin_id=23,
-            name="Example Court",
-            lat=0.0,
-            lon=0.0,
-        )
+        self.court1 = court_import(self.court1_json)
 
     def test_modify_court_name(self):
         c = Client()
@@ -24,3 +19,65 @@ class SearchTestCase(TestCase):
         new_court = Court.objects.get(name="New Name")
         self.assertEqual(new_court.admin_id, 23)
 
+    self.court1_json = """
+    {
+        "addresses": [
+            {
+                "town": "Accrington",
+                "type": "Visiting",
+                "postcode": "BB5 2BH",
+                "address": "East Lancashire Magistrates' Court\nThe Law Courts\nManchester Road\n"
+            },
+            {
+                "town": "Blackburn",
+                "type": "Postal",
+                "postcode": "BB2 1AA",
+                "address": "Accrington Magistrates' Court\nThe Court House \nNorthgate\t\n"
+            }
+        ],
+        "admin_id": 2800,
+        "lat": "53.7491281247251",
+        "slug": "accrington-magistrates-court",
+        "court_types": [
+            "Magistrates Court"
+        ],
+        "name": "Accrington Magistrates' Court",
+        "contacts": [
+            {
+                "sort": 2,
+                "type": "Fax",
+                "number": "0870 739 4254"
+            },
+            {
+                "sort": 0,
+                "type": "Enquiries",
+                "number": "01254  687500"
+            },
+            {
+                "sort": 1,
+                "type": "Fine queries",
+                "number": "01282  610000"
+            },
+            {
+                "sort": 3,
+                "type": "Witness service",
+                "number": "01254 265 305"
+            },
+            {
+                "sort": null,
+                "type": "DX",
+                "number": "742020 Blackburn 10"
+            }
+        ],
+        "court_number": 1725,
+        "lon": "-2.359323760375266",
+        "postcodes": [],
+        "areas_of_law": [
+            {
+                "councils": [],
+                "name": "Crime"
+            }
+        ]
+    },
+"""
+    self.countries
--- courtfinder/update/views.py
+++ courtfinder/update/views.py
@@ -5,21 +5,6 @@ from django.http import *
 from search.models import Court
 
 def court(request):
-    """
-    update a single court from data posted
-    """
-    if request.method != 'POST':
-        return HttpResponseNotAllowed(['POST'], "Only POST allowed")
-    court_data_json = request.POST.get('court_data', '')
-
-    try:
-        court_data = json.loads(court_data_json)
-    except ValueError:
-        return HttpResponseBadRequest('Invalid JSON data received: '+court_data_json)
-
-    court_to_change = Court.objects.get(admin_id=court_data['admin_id'])
-    court_to_change.name = court_data['name']
-    court_to_change.save()
 
     return HttpResponse('OK')
 
