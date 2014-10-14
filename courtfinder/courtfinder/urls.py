14b4a4f5bb2d8b0f52ffbd3394f5ad4a992507de
Max Froumentin <max.froumentin@digital.justice.gov.uk>
2014-10-14 16:40:16 +0100

    static error pages

--- courtfinder/courtfinder/urls.py
+++ courtfinder/courtfinder/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^$', include('staticpages.urls', namespace='staticpages')),
+    url(r'^errors/', include('staticpages.urls', namespace='staticpages')),
     url(r'^search/', include('search.urls', namespace='search')),
     url(r'^courts/', include('courts.urls', namespace='courts')),
 )
--- courtfinder/staticpages/urls.py
+++ courtfinder/staticpages/urls.py
@@ -3,4 +3,7 @@ from staticpages import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='home'),
+    # mock error pages, provided statically to nginx
+    url(r'^404$', views.notfound, name='notfound'),
+    url(r'^500$', views.error, name='error'),
 )
