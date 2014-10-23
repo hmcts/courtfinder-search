294e0f4c0b22feac5e65f0c9d1e0850117d82a00
Max Froumentin <max.froumentin@digital.justice.gov.uk>
2014-10-23 17:38:49 +0100

    fix wrong links in breadcrumbs

--- courtfinder/courtfinder/urls.py
+++ courtfinder/courtfinder/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-    url(r'^errors/', include('staticpages.urls', namespace='staticpages')),
     url(r'^search/', include('search.urls', namespace='search')),
     url(r'^courts/', include('courts.urls', namespace='courts')),
     url(r'^', include('staticpages.urls', namespace='staticpages')),
--- courtfinder/staticpages/urls.py
+++ courtfinder/staticpages/urls.py
@@ -4,7 +4,7 @@ from staticpages import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name='home'),
     # mock error pages, provided statically to nginx
-    url(r'^404$', views.notfound, name='notfound'),
-    url(r'^500$', views.error, name='error'),
+    url(r'^errors/404$', views.notfound, name='notfound'),
+    url(r'^errors/500$', views.error, name='error'),
     url(r'^api(.html)?$', views.api, name='api'),
 )
