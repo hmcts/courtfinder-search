0ecbd573e6429f156599d2da62e2c5655f04a35c
Max Froumentin <max.froumentin@digital.justice.gov.uk>
2014-10-23 16:08:44 +0100

    google analytics code (ga_id isn't used)

--- courtfinder/courtfinder/context_processors.py
+++ courtfinder/courtfinder/context_processors.py
@@ -6,5 +6,4 @@ def globals(request):
         'phase': 'beta', # Current Phase (Sets the current phase and the colour of phase tags). Presumed values: alpha, beta, live
         'product_type': 'service', # Product Type (Adds class to body based on service type). Presumed values: information, service
         'feedback_url': '', # Feedback URL (URL for feedback link in phase banner)
-        'ga_id': '' # Google Analytics ID (Tracking ID for the service)
      }
