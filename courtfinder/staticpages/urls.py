a98b71fcbfbbac3c1235b95befc4c5025b06c3a6
Max Froumentin <max.froumentin@digital.justice.gov.uk>
2014-10-28 18:41:26 +0000

    feedback form to email

--- courtfinder/staticpages/urls.py
+++ courtfinder/staticpages/urls.py
@@ -7,4 +7,6 @@ urlpatterns = patterns('',
     url(r'^errors/404$', views.notfound, name='notfound'),
     url(r'^errors/500$', views.error, name='error'),
     url(r'^api(.html)?$', views.api, name='api'),
+    url(r'^feedback$', views.feedback, name='feedback'),
+    url(r'^feedback-sent$', views.feedback_sent, name='feedback_sent'),
 )
