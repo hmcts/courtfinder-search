c48049e8e6beef5025eeb061732b748fddc897c0
Mehmet Duran <mehmet.duran@gmail.com>
2014-11-03 20:45:11 +0000

    More analytics

--- courtfinder/assets-src/javascripts/analytics.js
+++ courtfinder/assets-src/javascripts/analytics.js
@@ -20,11 +20,36 @@
   }
 
   if( $('#aol-page').length > 0 ){
-    $('#aol-page form[action^="/search/"]').submit(function (e){
+    $('#aol-page form[action^="/search/"]').submit(function (){
       var val = $('#aol-page label input:checked').val();
       var name = val.toLowerCase().replace(' ', '-');
 
       ga('send', 'event', 'aol-page', 'aol-' + name + '-selected', 'Area of Law selected: ' + val);
+    });
+  }
+
+  if( $('#spoe-page').length > 0 ){
+    $('#spoe-page form[action^="/search"]').submit(function (){
+      var selected = $('#spoe-page form label input:checked').val();
+      var label = selected == 'start' ? "Starting a new claim selected" : "Already in contact selected";
+
+      ga('send', 'event', 'spoe-page', 'spoe-' + selected + 'selected', label);
+    });
+  }
+
+  if( $('#postcode-page').length > 0 ){
+    $('#postcode-page #housing-possession-online-link').click(function (){
+      ga('send', 'event', 'postcode-page', 'housing-possession-online-link-clicked', 'Housing possession online link clicked');
+    });
+
+    $('#postcode-page #money-claims-online-link').click(function (){
+      ga('send', 'event', 'postcode-page', 'money-claims-online-link-clicked', 'Money claims online link clicked');
+    });
+  }
+
+  if( $('#search-results-page').length > 0 ){
+    $('#search-results-page .court-map a').click(function (){
+      ga('send', 'event', 'search-results-page', 'map-link-clicked', 'A map link clicked on search results');
     });
   }
 })(jQuery);
