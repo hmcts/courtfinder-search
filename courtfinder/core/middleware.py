dd9b9165633dffb07e4d23f65d3bb0de48bc4100
Mehmet Duran <mehmet.duran@gmail.com>
2014-11-05 15:34:32 +0000

    just give up

--- courtfinder/core/middleware.py
+++ courtfinder/core/middleware.py
@@ -15,13 +15,16 @@ class RequestLoggingMiddleware(object):
         self.logger = logging.getLogger('courtfinder.requests')
 
     def process_request(self, request):
-        self.logger.debug(json.dumps({
-            'time': strftime("%Y-%m-%d %H:%M:%S", gmtime()),
-            'fullPath': request.get_full_path(),
-            'path': request.path,
-            'method': request.method,
-            'get': request.GET,
-            'post': request.POST,
-            'userAgent': request.META['HTTP_USER_AGENT'],
-            'ipAddress': request.META['REMOTE_ADDR'],
-        }))
+        try:
+            self.logger.debug(json.dumps({
+                'time': strftime("%Y-%m-%d %H:%M:%S", gmtime()),
+                'fullPath': request.get_full_path(),
+                'path': request.path,
+                'method': request.method,
+                'get': request.GET,
+                'post': request.POST,
+                'userAgent': request.META['HTTP_USER_AGENT'],
+                'ipAddress': request.META['REMOTE_ADDR'],
+            }))
+        except:
+            pass
