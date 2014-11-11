59dee0102a3ad38e23f0c4ff1e49ce843db537e7
Max Froumentin <max.froumentin@digital.justice.gov.uk>
2014-11-11 14:51:14 +0000

    simpler paths

--- courtfinder/staticpages/tests.py
+++ courtfinder/staticpages/tests.py
@@ -7,10 +7,11 @@ from search.ingest import Ingest
 
 
 
+
 class SearchTestCase(TestCase):
 
     def setUp(self):
-        test_data_dir = settings.DJANGO_ROOT +  '/../../data/test_data/'
+        test_data_dir = settings.PROJECT_ROOT +  '/data/test_data/'
         countries_json_1 = open(test_data_dir + 'countries.json').read()
         courts_json_1 = open(test_data_dir + 'courts.json').read()
         Ingest.countries(json.loads(countries_json_1))
