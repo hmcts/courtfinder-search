6894dab8dc5b41ca3fe7ffea78648cda7e6edb20
Max Froumentin <max.froumentin@digital.justice.gov.uk>
2015-01-28 11:34:20 +0000

    improved

--- data/postcodes/research/jurisdiction-models-tests/run_regression_test.py
+++ data/postcodes/research/jurisdiction-models-tests/run_regression_test.py
@@ -18,7 +18,7 @@ cf_search_url = "http://127.0.0.1:8000/search/results.json?spoe=continue&postcod
 cf_old_url = "https://courttribunalfinder.service.gov.uk/search/results.json?spoe=continue&postcode=%s&aol=%s"
 
 # number of postcodes to try for each test
-num_postcodes = 100
+num_postcodes = int(sys.argv[1])
 
 # how long to sleep between postcodes (so as not to overload mapit)
 sleep_average = 0.25 # seconds
@@ -28,6 +28,7 @@ sleep_average = 0.25 # seconds
 def main():
     compare_for(['Bankruptcy', 'Housing possession', 'Money claims'], ['london'], 'london_civil')
     compare_for(['Bankruptcy', 'Housing possession', 'Money claims'], ['england', 'wales', 'scotland'], 'all_civil')
+
 
 
 def compare_for(aols, regions, test_name):
@@ -40,7 +41,7 @@ def compare_for(aols, regions, test_name):
             fhandle.close()
         all_postcodes += postcodes
 
-    test_postcodes( postcodes, aols, test_name )
+    test_postcodes( all_postcodes, aols, test_name )
 
 
 def test_postcodes( all_postcodes, areas_of_law, test_name ):
@@ -48,75 +49,73 @@ def test_postcodes( all_postcodes, areas_of_law, test_name ):
     print('========================================')
     print('running postcodes for ' + test_name)
 
-    passed = []
-    failed = []
-    failed_lookups = 0
+
+    same_results = []
+    different_results = []
+    failed_lookups = []
 
     postcodes = random.sample(all_postcodes, num_postcodes)
 
+    total_postcodes = len(postcodes)
+    tested_postcodes = 0
+
     for postcode in postcodes:
         for area_of_law in areas_of_law:
-            old_results = make_old_cf_search(postcode, area_of_law)
-            new_results = make_new_cf_search(postcode, area_of_law)
+            old_results = cf_search(postcode, area_of_law, cf_old_url)
+            new_results = cf_search(postcode, area_of_law, cf_search_url)
 
             if len(old_results) > 0 and len(new_results) > 0:
                 if old_results[0] == new_results[0]:
-                    sys.stdout.write('+')
 
-                    passed.append({
+                    same_results.append({
                         "postcode": postcode,
+                        "area_of_law": area_of_law,
                         "old_results": old_results,
                         "new_results": new_results
                     })
                 else:
-                    sys.stdout.write('-')
 
-                    failed.append({
+                    different_results.append({
                         "postcode": postcode,
+                        "area_of_law": area_of_law,
                         "old_results": old_results,
                         "new_results": new_results
                     })
 
             else:
-                sys.stdout.write('*')
-                failed_lookups = failed_lookups + 1
+                failed_lookups.append({
+                    "postcode": postcode,
+                    "area_of_law": area_of_law
+                })
+
             sys.stdout.flush()
             time.sleep(random.uniform(sleep_average - .15, sleep_average + 0.15))
 
+        tested_postcodes += 1
+        num_results = len(same_results)+len(different_results)
+        ratio = float(len(same_results))/float(num_results) if num_results else 0
+        sys.stdout.write("Tested %d / %d postcodes. %.2f success ratio so far\r" % (tested_postcodes, total_postcodes, ratio))
+        sys.stdout.flush()
 
     print "\nTest complete."
 
-    total_count = (len(passed) + len(failed))
+    total_count = (len(same_results) + len(different_results))
 
-    print "Tested %d random postcodes on %d areas of law." % (len(postcodes), len(areas_of_law))
-    print "%s postcodes gave the same results." % len(passed)
-    print "%s postcodes gave different results." % len(failed)
-    print "%s failures (probably mapit overload)." % failed_lookups
-    print "%.2f%% success rate." % (float(len(passed))/float(total_count)*100)
+    print "Tested %d random postcodes on %d areas of law (total: %d postcodes)." % (len(postcodes), len(areas_of_law), len(postcodes)*len(areas_of_law))
+    print "%s postcodes gave the same results." % len(same_results)
+    print "%s postcodes gave different results." % len(different_results)
+    print "%s failures (probably mapit overload)." % len(failed_lookups)
+    print "%.2f%% same/total ratio." % (float(len(same_results))/float(total_count)*100)
 
-    generate_log(test_name, passed, failed)
+    generate_log(test_name, same_results, different_results, failed_lookups)
 
 
-def make_old_cf_search( postcode, area_of_law ):
-    url = cf_old_url % (format_postcode( postcode ), area_of_law)
+def cf_search( postcode, area_of_law, endpoint ):
+    url = endpoint % (format_postcode( postcode ), area_of_law)
     r = requests.get(url, verify=False)
     if r.status_code == 200:
         response = json.loads(r.text)
-        results = [result['name'] for result in response]
-
-        return results
-    else:
-        return []
-
-
-def make_new_cf_search( postcode, area_of_law ):
-    url = cf_search_url % (format_postcode(postcode), area_of_law)
-    r = requests.get(url, verify=False)
-    if r.status_code == 200:
-        response = json.loads(r.text)
-        results = [result['name'] for result in response]
-
-        return results
+        return [result['name'] for result in response]
     else:
         return []
 
@@ -125,19 +124,21 @@ def format_postcode( postcode ):
     return postcode.replace(' ', '+')
 
 
-def generate_log( test_type, passed, failed ):
-    total_count = len(passed) + len(failed)
+def generate_log( test_type, same_results, different_results, failed_lookups ):
+    total_count = len(same_results) + len(different_results)
 
     log_obj = {
         "stats": {
             "total postcodes": total_count,
-            "same results": len(passed),
-            "different results": len(failed),
-            "success_rate": round((float(len(passed))/float(total_count)*100), 2)
+            "same results": len(same_results),
+            "different results": len(different_results),
+            "failed lookups": len(failed_lookups),
+            "success_rate": round((float(len(same_results))/float(total_count)*100), 2)
         },
         "log": {
-            "same results": passed,
-            "different results": failed
+            "same results": same_results,
+            "different results": different_results,
+            "failed lookups": failed_lookups
         }
     }
 
