676d80bb9cb242dfd41e451916d7af830d483b88
Stuart Munro <stuart@munro.co>
2015-02-03 12:58:30 +0000

    wrong db name

--- docker/setup_postgresql.sh
+++ docker/setup_postgresql.sh
@@ -3,7 +3,7 @@
 /etc/init.d/postgresql start
 /usr/bin/psql -c 'CREATE ROLE courtfinder LOGIN SUPERUSER INHERIT CREATEDB CREATEROLE REPLICATION;' -U postgres
 /usr/bin/psql -c 'CREATE DATABASE "courtfinder_search";' -U postgres
-/usr/bin/psql -c "ALTER USER courtfinder_search WITH PASSWORD '123456';" -U postgres
+/usr/bin/psql -c "ALTER USER courtfinder WITH PASSWORD '123456';" -U postgres
 PGPASSWORD=123456 /usr/bin/psql courtfinder_search -c 'CREATE EXTENSION postgis;' -U courtfinder
 PGPASSWORD=123456 /usr/bin/psql courtfinder_search -c 'CREATE EXTENSION postgis_topology;' -U courtfinder
 
