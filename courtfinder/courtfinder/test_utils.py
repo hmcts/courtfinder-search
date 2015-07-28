f9bfb6675dec2c9cd3d9a07a05bba2dcefb81e04
Igor <igor.ushkarev@digital.justice.gov.uk>
2015-07-28 13:17:29 +0100

    test case data is now populated by the populate-db management command populate-db cleaned up; works identically

--- courtfinder/courtfinder/test_utils.py
+++ courtfinder/courtfinder/test_utils.py
@@ -1,15 +1,7 @@
-import json
-import os
+from django.core.management import call_command
 from django.test import TestCase
-from django.conf import settings
-from search.ingest import Ingest
-from search.models import DataStatus
 
 
 class TestCaseWithData(TestCase):
     def setUp(self):
-        test_data_path = os.path.join(settings.PROJECT_ROOT, 'data', 'test_data', 'courts.json')
-        with open(test_data_path) as f:
-            courts_json = f.read()
-        Ingest.courts(json.loads(courts_json))
-        DataStatus.objects.create(data_hash='6f115002ec6ed1745df7d676d10030fe')
+        call_command('populate-db', 'data/test_data', verbosity='0')
