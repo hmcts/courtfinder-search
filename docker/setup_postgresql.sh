59b9efaa0f9e55b4a3d743d6df39628433803a48
Lukasz Raczylo <lukasz@raczylo.com>
2015-09-21 17:12:23 +0100

    make python app use the same database host

--- docker/setup_postgresql.sh
+++ docker/setup_postgresql.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-/usr/bin/psql -c 'CREATE DATABASE IF NOT EXISTS "courtfinder_production" WITH OWNER courtfinder_search;' -h $DB_HOST
-/usr/bin/psql -c 'CREATE DATABASE IF NOT EXISTS "courtfinder_search" WITH OWNER courtfinder_search;' -h $DB_HOST
-PGPASSWORD=C1cwG3P7n2 /usr/bin/psql courtfinder_search -c 'CREATE EXTENSION IF NOT EXISTS postgis;' -h $DB_HOST
-PGPASSWORD=C1cwG3P7n2 /usr/bin/psql courtfinder_search -c 'CREATE EXTENSION IF NOT EXISTS postgis_topology;' -h $DB_HOST
+PGPASSWORD=C1cwG3P7n2 /usr/bin/psql -c 'CREATE DATABASE IF NOT EXISTS "courtfinder_production" WITH OWNER courtfinder_search;' -h $DB_HOST -U courtfinder
+PGPASSWORD=C1cwG3P7n2 /usr/bin/psql -c 'CREATE DATABASE IF NOT EXISTS "courtfinder_search" WITH OWNER courtfinder_search;' -h $DB_HOST -U courtfinder
+PGPASSWORD=C1cwG3P7n2 /usr/bin/psql courtfinder_search -c 'CREATE EXTENSION IF NOT EXISTS postgis;' -h $DB_HOST -U courtfinder
+PGPASSWORD=C1cwG3P7n2 /usr/bin/psql courtfinder_search -c 'CREATE EXTENSION IF NOT EXISTS postgis_topology;' -h $DB_HOST -U courtfinder
 
 # Importing database
+cd courtfinder
 python manage.py makemigrations
 python manage.py migrate
 python manage.py populate-db
