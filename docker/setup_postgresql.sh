4370a189cc99476d81dc663101918de634dbe187
Lukasz Raczylo <lukasz@raczylo.com>
2015-09-25 13:04:59 +0100

    Drop database during deployment process as a test

--- docker/setup_postgresql.sh
+++ docker/setup_postgresql.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
-PGPASSWORD=C1cwG3P7n2 /usr/bin/psql -c 'CREATE DATABASE IF NOT EXISTS "courtfinder_production" WITH OWNER courtfinder_search;' -h $DB_HOST -U courtfinder
-PGPASSWORD=C1cwG3P7n2 /usr/bin/psql -c 'CREATE DATABASE IF NOT EXISTS "courtfinder_search" WITH OWNER courtfinder_search;' -h $DB_HOST -U courtfinder
-PGPASSWORD=C1cwG3P7n2 /usr/bin/psql courtfinder_search -c 'CREATE EXTENSION IF NOT EXISTS postgis;' -h $DB_HOST -U courtfinder
-PGPASSWORD=C1cwG3P7n2 /usr/bin/psql courtfinder_search -c 'CREATE EXTENSION IF NOT EXISTS postgis_topology;' -h $DB_HOST -U courtfinder
+EXPORT PGPASSWORD=C1cwG3P7n2
+
+/usr/bin/psql -c 'DROP DATABASE IF EXISTS "courtfinder_search;' -h $DB_HOST -U courtfinder
+/usr/bin/psql -c 'CREATE DATABASE IF NOT EXISTS "courtfinder_production" WITH OWNER courtfinder_search;' -h $DB_HOST -U courtfinder
+/usr/bin/psql -c 'CREATE DATABASE IF NOT EXISTS "courtfinder_search" WITH OWNER courtfinder_search;' -h $DB_HOST -U courtfinder
+/usr/bin/psql courtfinder_search -c 'CREATE EXTENSION IF NOT EXISTS postgis;' -h $DB_HOST -U courtfinder
+/usr/bin/psql courtfinder_search -c 'CREATE EXTENSION IF NOT EXISTS postgis_topology;' -h $DB_HOST -U courtfinder
 
 # Importing database
 cd /srv/search/courtfinder
