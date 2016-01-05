59f9047934f5791c2a2434839ca8c3086f52d633
Steve Marshall <steve@stevemarshall.com>
2016-01-05 13:55:57 +0000

    Use standard ping.json from django-moj-irat
    [django-moj-irat](https://github.com/ministryofjustice/django-moj-irat)
    offers a ready-made, well-tested `ping.json` implementation that
    behaves in the way we want (using environment variables to provide its
    values), so we should use that instead of maintaining our own.
    
    This depends on ministryofjustice/django-moj-irat#4, which adds support
    for Python 2.7.
    
    For this to behave as intended, we need `APP_BUILD_DATE`,
    `APP_GIT_COMMIT`, `APP_BUILD_TAG`, and `APP_VERSION` environment
    variables to be appropriately set.

--- courtfinder/apps/healthcheck/urls.py
+++ courtfinder/apps/healthcheck/urls.py
@@ -1,7 +1,14 @@
 from django.conf.urls import patterns, url
 
+from moj_irat.views import PingJsonView
+
 urlpatterns = patterns('healthcheck.views',
-    url(r'^ping.json$', 'ping'),
+    url(r'^ping.json$', PingJsonView.as_view(
+        build_date_key='APP_BUILD_DATE',
+        commit_id_key='APP_GIT_COMMIT',
+        build_tag_key='APP_BUILD_TAG',
+        version_number_key='APP_VERSION',
+    ), name='ping_json'),
     url(r'^healthcheck.json$', 'healthcheck'),
     url(r'^pingdom-search-statistics$', 'pingdom_search_statistics'),
 )
