dce7b85b4cd7cbeef1513c81c7354cf8004cb44f
Steve Marshall <steve@stevemarshall.com>
2016-01-05 17:34:09 +0000

    Reimplement data freshness healthcheck
    This re-adds a healthcheck, using the new `moj_irat` framework to warn
    if the current data in the system is both different to the latest
    version on the remote server and older than 10 minutes; if that's the
    case, it would suggest something is wrong with the ingestion.
    
    This is also much more thoroughly tested in an attempt to more clearly
    document the expected behaviour.
    
    There's a slight bastardisation of `UrlHealthcheck.success_response`
    here: we're making it return a failure response if the data is stale,
    but I couldn't see a better way to do this without having to
    significantly reimplement `UrlHealthcheck.__call__` in
    `CourtDataFreshnessHealthcheck`.

--- courtfinder/apps/healthcheck/views.py
+++ courtfinder/apps/healthcheck/views.py
@@ -12,40 +12,6 @@ from dateutil import tz
 from search.models import SearchStatistic
 
 
-def healthcheck(request):
-    """
-    IRaT healthcheck.json: returns status of dependency services
-    """
-    response = {
-        's3_courts_data': {
-            'status': 'DOWN',
-        },
-    }
-
-    try:
-        assert settings.COURTS_DATA_S3_URL, 'S3 url for courts.json data not known'
-        r = requests.get(settings.COURTS_DATA_S3_URL, stream=True, timeout=10)
-        assert r.status_code == 200, 'S3 url for courts.json data did not return 200'
-        digest = hashlib.md5()
-        for chunk in r.iter_content(1024):
-            digest.update(chunk)
-        response['s3_courts_data']['s3_url'] = settings.COURTS_DATA_S3_URL
-        response['s3_courts_data']['s3_hash'] = digest.hexdigest()
-        from search.models import DataStatus
-        last_ingestion = DataStatus.objects.all().order_by('-last_ingestion_date').first()
-        response['s3_courts_data']['local_hash'] = last_ingestion.data_hash
-        response['s3_courts_data']['local_date'] = last_ingestion.last_ingestion_date
-        assert response['s3_courts_data']['s3_hash'] == last_ingestion.data_hash \
-            or timezone.now() - last_ingestion.last_ingestion_date <= datetime.timedelta(minutes=10), \
-            'Local courts data is different from S3 and older than 10min'
-        response['s3_courts_data']['status'] = 'UP'
-    except (requests.RequestException, Exception) as e:
-        response['s3_courts_data']['error'] = unicode(e)
-
-    fully_working = all(item['status'] == 'UP' for item in response.values())
-    return JsonResponse(response, status=200 if fully_working else 503)
-
-
 def pingdom_search_statistics(request):
     """
     Pingdom custom HTTP check endpoint for IRaT threshold alert
