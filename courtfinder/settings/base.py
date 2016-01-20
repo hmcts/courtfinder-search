f1fcd35e74c6541ebc2381dd287b5b20ff9ce0c0
Niall Creech <niall.creech@digital.justice.gov.uk>
2016-01-20 12:30:35 +0000

    Add missing sys.stdout import

--- courtfinder/settings/base.py
+++ courtfinder/settings/base.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 from os.path import abspath, basename, dirname, join
-from sys import path
+from sys import path, stdout
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
