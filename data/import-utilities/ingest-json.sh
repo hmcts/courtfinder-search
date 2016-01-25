8c63a8ad059598be4bb47a9b9adf7df13ee3b2fd
Niall Creech <niall.creech@digital.justice.gov.uk>
2016-01-25 11:32:01 +0000

    Fix populate database script
    * Add boto3 to requirements
    * Update script to connect to S3 through boto3
    * Use logging
    * Updated ingest script to use defines DB_* environment variables

--- data/import-utilities/ingest-json.sh
+++ data/import-utilities/ingest-json.sh
@@ -1,12 +1,34 @@
 #!/bin/bash
-PYTHON=env/bin/python
-DBNAME='courtfinder_search'
-DBNAMETMP='courtfinder_search_tmp'
-DBUSER='mf'
-DBADMINUSER='mf'
+PYTHON=python
 
-psql -U $DBUSER template1 -c "CREATE DATABASE $DBNAMETMP WITH TEMPLATE $DBNAME OWNER $DBUSER;"
-$PYTHON manage.py populate-db || { echo 'populate-db failed'; exit -1; }
-psql -U $DBUSER template1 -c "DROP DATABASE $DBNAME; ALTER DATABASE $DBNAMETMP RENAME TO $DBNAME;"
-echo 'Done.'
+# Name of the temp database to be used to store data during import
+DB_NAME_TMP="${DB_NAME}_tmp"
 
+# Setup postgres pasword and args
+export PGPASSWORD=${DB_PASSWORD}
+PSQL_ARGS="-h ${DB_HOST} -p ${DB_PORT} -U ${DB_USERNAME} template1"
+
+echo Dropping any temporary database if it exists
+psql ${PSQL_ARGS} -c "DROP DATABASE $DB_NAME_TMP;"
+
+
+echo Populating temporary database...
+$PYTHON manage.py populate-db --load-remote
+if [ $? -eq 0 ]; then
+	echo Creating temporary database...
+	psql ${PSQL_ARGS} -c "CREATE DATABASE $DB_NAME_TMP WITH TEMPLATE $DB_NAME OWNER $DB_USERNAME;"
+	$PYTHON manage.py populate-db --ingest
+	if [ $? -eq 0 ]; then 
+		echo Replacing ${DB_NAME} with newly populated database ${DB_NAME_TMP}...
+		psql ${PSQL_ARGS} -c "DROP DATABASE $DB_NAME;"
+		psql ${PSQL_ARGS} -c "ALTER DATABASE $DB_NAME_TMP RENAME TO $DB_NAME;"
+		echo 'Done.'
+	else
+	  echo "Failed ingesting files." >&2
+	  psql ${PSQL_ARGS} -c "DROP DATABASE $DB_NAME;"
+	  exit 1
+	fi
+else
+	  echo "Failed loading files." >&2
+	  exit 1
+fi
