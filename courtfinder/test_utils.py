e6eebee74182a161839cf61f30bd19eb248412d7
Niall Creech <niall.creech@digital.justice.gov.uk>
2016-01-26 15:02:27 +0000

    Fix test utils call to populate-db
    populate-db now requires options to be passed such as --load-remote
    or --ingest, this change updates the test calls to reflect this
    change.

--- courtfinder/test_utils.py
+++ courtfinder/test_utils.py
@@ -4,4 +4,4 @@ from django.test import TestCase
 
 class TestCaseWithData(TestCase):
     def setUp(self):
-        call_command('populate-db', 'data/test_data', verbosity='0')
+        call_command('populate-db', 'data/test_data', verbosity='0', ingest='0')
