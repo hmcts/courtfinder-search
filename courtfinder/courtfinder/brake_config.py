d63fefd199b9408c07b7d90b6cc24479116afc04
Lyndon Garvey <lyndon.garvey@digital.justice.gov.uk>
2016-07-18 15:37:06 +0100

    AWS ELB X-forwarded-for IP has both the public and private IP. Make sure we use only the public IP

--- courtfinder/courtfinder/brake_config.py
+++ courtfinder/courtfinder/brake_config.py
@@ -3,6 +3,10 @@ from brake.backends import cachebe
 
 class ELBBrake(cachebe.CacheBackend):
     def get_ip(self, request):
-        return request.META.get(
-            'HTTP_X_FORWARDED_FOR',
-            request.META.get('REMOTE_ADDR'))
+
+        import pdb; pdb.set_trace()
+
+        try:
+            return request.META.get('HTTP_X_FORWARDED_FOR').split(",")[0]
+        except (AttributeError, IndexError):
+            return request.META.get('REMOTE_ADDR')
