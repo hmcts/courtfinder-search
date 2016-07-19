2bd834e2eeecbd835bebfeb0b39a8de6b2d93d99
Lyndon Garvey <lyndon.garvey@digital.justice.gov.uk>
2016-07-19 13:21:50 +0100

    Remove rogue pdb statement

--- courtfinder/courtfinder/brake_config.py
+++ courtfinder/courtfinder/brake_config.py
@@ -4,8 +4,6 @@ from brake.backends import cachebe
 class ELBBrake(cachebe.CacheBackend):
     def get_ip(self, request):
 
-        import pdb; pdb.set_trace()
-
         try:
             return request.META.get('HTTP_X_FORWARDED_FOR').split(",")[0]
         except (AttributeError, IndexError):
