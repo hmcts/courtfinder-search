5e68fb7789fd91610c74710b2de3359e972c50c1
andrew harland <andrew.harland@cgi.com>
2017-01-12 11:33:35 +0000

    Based on master!
    Contains a model update.
    
    Discovery phase changes - DX number moved, Court code search added, Areas of Law external URL added and breadcrumbs updated.
    
    DX number - The 'Legal professionals' block has been removed and the DX number and court number moved to the 'Visit Us' block.
    Court code - A new search option has been added to allow users to search specifically on the court number; breadcrumbs have also been updated.
    AoL - Links have been applied to these with external links to Government sites. This involves a model update!
    Breadcrumbs - The 'Court list' crumb has been changed to 'A-Z list of all courts'

---
+++ courtfinder/search/tests/test_courtcode.py
@@ -0,0 +1,40 @@
+import json
+from django.test import TestCase, Client
+from search.models import *
+from django.conf import settings
+from search.ingest import Ingest
+
+
+class CourtCodeTestCase(TestCase):
+
+    def setUp(self):
+        test_data_dir = settings.PROJECT_ROOT + '/data/test_data/'
+        courts_json_1 = open(test_data_dir + 'courts.json').read()
+        Ingest.courts(json.loads(courts_json_1))
+        DataStatus.objects.create(data_hash='415d49233b8592cf5195b33f0eddbdc86cebc72f2d575d392e941a53c085281a')
+
+    def tearDown(self):
+        pass
+
+    def test_non_existing(self):
+        """
+        Only numerics
+        """
+        c = Client()
+
+        response = c.get('/search/results?courtcode=123', follow=True)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('validation-error', response.content)
+
+    def test_exists(self):
+        """
+        Only numerics
+        """
+        c = Client()
+
+        response = c.get('/search/results?courtcode=1725', follow=True)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("Accrington", response.content)
+        self.assertNotIn('validation-error', response.content)
