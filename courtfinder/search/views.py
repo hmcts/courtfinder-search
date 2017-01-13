997296ad61a63cc6885c36ea0876c768e936b4a5
Lyndon Garvey <lyndon.garvey@digital.justice.gov.uk>
2017-01-13 12:34:15 +0000

    Fix AOL error
    Prevent mismatches from DB and code AOL lists from causing errors

--- courtfinder/search/views.py
+++ courtfinder/search/views.py
@@ -38,7 +38,10 @@ def aol(request):
     areas_of_law = AreaOfLaw.objects.all().exclude(name='High court').order_by('name')
     aol = request.GET.get('aol', 'All')
     for area in areas_of_law:
-        area.description = areas_of_law_description[area.name]
+        try:
+            area.description = areas_of_law_description[area.name]
+        except KeyError:
+            pass
     return render(request, 'search/aol.jinja', {
         'areas_of_law': areas_of_law, 'aol': aol,
     })
