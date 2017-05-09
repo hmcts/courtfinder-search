01cd12fed2d642264ae8d1eecd3a89358c7fc6d4
Alexa <alexaballantine@Alexas-MacBook-Pro.local>
2017-05-09 11:14:44 +0100

    Added tests for all courts search page

--- cucumber/features/step_definitions/name_or_address_steps.rb
+++ cucumber/features/step_definitions/name_or_address_steps.rb
@@ -13,15 +13,15 @@ Then(/^I should see the hint for the form$/) do
     .to eq 'Enter one of the following'
 end
 
-When(/^I search for '(.*?)'$/) do |string|
-  name_or_address_page.form_block.address.set(string)
+When(/^I search for '(.*?)'$/) do |search_term|
+  name_or_address_page.form_block.address.set(search_term)
   name_or_address_page.continue_button.click
 end
 
-Then(/^I should see the result is '(.*?)'$/) do |string|
+Then(/^I should see the result is '(.*?)'$/) do |result|
   expect(search_results_page.search_results.number_of_results.text).to eq '1'
   expect(search_results_page.search_results.court_results.header[0].text)
-    .to eq string
+    .to eq result
 end
 
 Then(/^I should see the top result is Liverpool Civil and Family Court$/) do
@@ -30,7 +30,7 @@ Then(/^I should see the top result is Liverpool Civil and Family Court$/) do
     .to eq 'Liverpool Civil and Family Court'
 end
 
-Then(/^I should see '(.*?)' error message$/) do |string|
+Then(/^I should see '(.*?)' error message$/) do |error|
   expect(name_or_address_page.validation_error.text)
-    .to have_content string
+    .to have_content error
 end
