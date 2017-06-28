abfa0ec7b9d65bdc0d76db9ea20ccd3b943077ed
Alexa <alexaballantine@Alexas-MacBook-Pro.local>
2017-06-28 14:09:58 +0100

    RST-221 removed webmock as not compatible with Python

---
+++ features/court_page.feature
@@ -0,0 +1,6 @@
+Feature: Court details page
+
+  Background: Navigating to the search results page
+    Given I visit the search results page
+
+  Scenario:
--- features/step_definitions/postcode_page_steps.rb
+++ features/step_definitions/postcode_page_steps.rb
@@ -18,25 +18,25 @@ Then(/^I should see postcode error message$/) do
 end
 
 When(/^I enter a full valid postcode$/) do
-  postcode_page.postcode.set('ig12bn')
+  postcode_page.postcode.set('SW1A 2AA')
   postcode_page.continue_button.click
 end
 
 Then(/^I should see results for that postcode$/) do
   result = postcode_page.search_results
 
-  expect(result.text).to eq 'These are the 10 courts or tribunals nearest ig12bn.'
+  expect(result.text).to eq 'These are the 10 courts or tribunals nearest SW1A 2AA.'
   expect(postcode_page.court_results.result.count).to eq 10
 end
 
 When(/^I enter a first half of a valid postcode$/) do
-  postcode_page.postcode.set('ig1')
+  postcode_page.postcode.set('SW1A')
   postcode_page.continue_button.click
 end
 
 Then(/^I should see results for the first half of the postcode$/) do
   result = postcode_page.search_results
 
-  expect(result.text).to eq 'These are the 10 courts or tribunals nearest ig1.'
+  expect(result.text).to eq 'These are the 10 courts or tribunals nearest SW1A.'
   expect(postcode_page.court_results.result.count).to eq 10
 end
