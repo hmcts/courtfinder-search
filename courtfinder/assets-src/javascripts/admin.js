6ada3d2e387d901f49b5f2e4266a7c5abf1f0d4c
Karol Grecki <karol.grecki@solirius.com>
2018-02-12 11:43:22 +0000

    UI tweaks for court list page

---
+++ courtfinder/assets-src/javascripts/admin.js
@@ -0,0 +1,38 @@
+(function ($) {
+  'use strict';
+
+  /* forms */
+  tinymce.init({
+    selector: '.rich-editor',
+    width: 784,
+    plugins : 'autolink link',
+    menubar: '',
+    toolbar: 'undo redo | bold italic underline strikethrough | link | removeformat',
+    statusbar: false,
+  });
+
+  setTimeout(function(){
+    $('.form-feedback').fadeOut(1000);
+  }, 3000);
+
+
+  /* court list */
+  $('#closed-court-toggle').change(function(){
+    $('tr.closed-court').toggleClass('hidden');
+  })
+
+  //add case insensitive contains
+  jQuery.expr[":"].icontains = jQuery.expr.createPseudo(function (arg) {
+    return function (elem) {
+      return jQuery(elem).text().toUpperCase().indexOf(arg.toUpperCase()) >= 0;
+    };
+  });
+
+  $('#court-search-box').on('input',function(e){
+    var search = $(this).val();
+    $('#closed-court-toggle').prop('checked', true).prop('disabled', search);
+    $('#court-list tr:not(:first-child)').addClass('hidden');
+    $("#court-list td:first-child:icontains('" + search + "')").parent().removeClass('hidden');
+  })
+
+})(jQuery);
