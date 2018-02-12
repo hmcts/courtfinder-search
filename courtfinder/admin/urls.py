fd8954105bd9b0be8d39e51783903c7411c3df01
Karol Grecki <karol.grecki@solirius.com>
2018-02-12 14:52:34 +0000

    Change court url scheme

--- courtfinder/admin/urls.py
+++ courtfinder/admin/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
     url(r'^auth/', include('django.contrib.auth.urls')),
     url(r'^courts$', views.courts, name='courts'),
     url(r'^court/(?P<id>[0-9]+)$', views.edit_court, name='court'),
-    url(r'^court/address/(?P<id>[0-9]+)$', views.edit_address, name='address'),
-    url(r'^court/address/(?P<id>[0-9]+)/(?P<address_id>[0-9]+)$', views.edit_address, name='address'),
-    url(r'^court/delete_address/(?P<id>[0-9]+)/(?P<address_id>[0-9]+)$', views.delete_address, name='delete_address'),
+    url(r'^court/(?P<id>[0-9]+)/address$', views.edit_address, name='address'),
+    url(r'^court/(?P<id>[0-9]+)/address/(?P<address_id>[0-9]+)$', views.edit_address, name='address'),
+    url(r'^court/(?P<id>[0-9]+)/delete_address/(?P<address_id>[0-9]+)$', views.delete_address, name='delete_address'),
     url(r'^users$', views.users, name='users'),
     url(r'^$', RedirectView.as_view(pattern_name=settings.LOGIN_REDIRECT_URL))
 ]
