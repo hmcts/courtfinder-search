15e58ef474416f5f7c063a51594e2f5c67e39629
Karol Grecki <karol.grecki@solirius.com>
2018-06-22 10:12:18 +0100

    Redirect old admin url to new

--- courtfinder/courtfinder/urls.py
+++ courtfinder/courtfinder/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import include, url
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     url(r'^search/', include('search.urls', namespace='search')),
@@ -6,4 +7,5 @@ urlpatterns = [
     url(r'^', include('staticpages.urls', namespace='staticpages')),
     url(r'^', include('healthcheck.urls', namespace='healthcheck')),
     url(r'^staff/', include('admin.urls', namespace='admin')),
+    url(r'^admin/$', RedirectView.as_view(pattern_name='admin:courts', permanent=True))
 ]
