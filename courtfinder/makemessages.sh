e93dd5a9a209079e3ca9d6ea8d137fbb0d595ec6
Karol Grecki <karol.grecki@solirius.com>
2018-06-26 13:32:32 +0100

    Turn on locale and translation for cy & en

---
+++ courtfinder/makemessages.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# Regenerate the language (.po) files based on strings marked
+# for translation. This should be re-run every time the templates
+# or language files are changed.
+
+python ./manage.py makemessages --locale=en --no-wrap -e jinja,py
+python ./manage.py makemessages --locale=cy --no-wrap -e jinja,py
