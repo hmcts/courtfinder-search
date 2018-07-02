5623be75f76ce1c30c141cff1c713182975d28e0
Karol Grecki <karol.grecki@solirius.com>
2018-07-02 09:59:59 +0100

    Translation tags for base templates

--- courtfinder/makemessages.sh
+++ courtfinder/makemessages.sh
@@ -4,5 +4,5 @@
 # for translation. This should be re-run every time the templates
 # or language files are changed.
 
-python ./manage.py makemessages --locale=en --no-wrap -e jinja,py
-python ./manage.py makemessages --locale=cy --no-wrap -e jinja,py
+python ./manage.py makemessages --locale=en --no-wrap -e jinja,py,html
+python ./manage.py makemessages --locale=cy --no-wrap -e jinja,py,html
