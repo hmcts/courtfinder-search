dcc45b7775b3f34add97763e0ece7330ecedf8c8
Karol Grecki <karol.grecki@solirius.com>
2018-07-17 13:14:53 +0100

    Json representation of court data

--- courtfinder/courts/urls.py
+++ courtfinder/courts/urls.py
@@ -4,6 +4,7 @@ from courts import views
 urlpatterns = [
 	url(r'^(?P<slug>.+)/leaflet/(?P<leaflet_type>.+)$', views.leaflet, name='leaflet'),
     url(r'^(?P<first_letter>[A-Z])?$', views.list, name='list'),
-    url(r'^(?P<slug>.+)$', views.court, name='court'),  
+    url(r'^(?P<slug>.+)\.json$', views.court_json, name='api-court'),
+    url(r'^(?P<slug>.+)$', views.court, name='court'),
 ]
 
