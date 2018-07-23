3f3d01bd1291a10f6bf0e79e4d7cb5f03edb4750
Karol Grecki <karol.grecki@solirius.com>
2018-07-23 15:52:25 +0100

    Fix admin redirect to match full url

--- courtfinder/courtfinder/urls.py
+++ courtfinder/courtfinder/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     url(r'^', include('staticpages.urls', namespace='staticpages')),
     url(r'^', include('healthcheck.urls', namespace='healthcheck')),
     url(r'^staff/', include('admin.urls', namespace='admin')),
-    url(r'^admin/$', RedirectView.as_view(pattern_name='admin:courts', permanent=True)),
+    url(r'^admin/', RedirectView.as_view(pattern_name='admin:courts', permanent=True)),
     url(r"^change-language/$", views.set_language, name="set_language"),
 ]
