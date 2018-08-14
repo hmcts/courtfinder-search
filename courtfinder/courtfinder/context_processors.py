3339949d3d201221f896c11c1487b715ecc83210
Karol Grecki <karol.grecki@solirius.com>
2018-08-14 10:19:29 +0100

    Translate title

--- courtfinder/courtfinder/context_processors.py
+++ courtfinder/courtfinder/context_processors.py
@@ -1,10 +1,10 @@
 from django.conf import settings
-
+from django.utils.translation import gettext as _
 
 def globals(request):
     return {
-        'app_title': 'Court and tribunal finder', # Application Title (Populates <title>)
-        'proposition_title': 'Court and tribunal finder', # Proposition Title (Populates proposition header)
+        'app_title': _('Court and tribunal finder'), # Application Title (Populates <title>)
+        'proposition_title': _('Court and tribunal finder'), # Proposition Title (Populates proposition header)
         'phase': 'beta', # Current Phase (Sets the current phase and the colour of phase tags). Presumed values: alpha, beta, live
         'product_type': 'service', # Product Type (Adds class to body based on service type). Presumed values: information, service
         'welsh_feature': settings.FEATURE_WELSH_ENABLED
