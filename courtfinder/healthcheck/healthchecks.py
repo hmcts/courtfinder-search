c50eeba13d1f1f25f8c3c071826939b6651667a0
Karol Grecki <karol.grecki@solirius.com>
2019-01-09 12:58:42 +0000

    Use api key in mapit healthcheck

--- courtfinder/healthcheck/healthchecks.py
+++ courtfinder/healthcheck/healthchecks.py
@@ -2,9 +2,9 @@ from django.conf import settings
 
 from moj_irat.healthchecks import UrlHealthcheck, registry
 
-
 registry.register_healthcheck(UrlHealthcheck(
     name='mapit',
     url='%sSW1A+1AA' % settings.MAPIT_BASE_URL,
     value_at_json_path=('SW1A 1AA', 'postcode'),
+    headers={'X-Api-Key': settings.MAPTI_API_KEY}
 ))
