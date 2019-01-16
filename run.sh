3c8fa2f6199348ad7604fa6f8e549cf49f99f67a
Max Awan <max.awan@digital.justice.gov.uk>
2019-01-16 15:08:37 +0000

    Remove ingest related files and references

--- run.sh
+++ run.sh
@@ -6,15 +6,10 @@ migrate)
     echo "Docker state set to ${DOCKER_STATE}, running migrate commands..."
     python courtfinder/manage.py migrate
     ;;
-seed)
-    echo "Docker state set to ${DOCKER_STATE}, running seed commands..."
-    python courtfinder/manage.py populate-db
-    ;;
 create)
     echo "Docker state set to ${DOCKER_STATE}, running create commands..."
     python courtfinder/manage.py makemigrations
     python courtfinder/manage.py migrate
-    python courtfinder/manage.py populate-db
     ;;
 *)
     echo "Docker state set to ${DOCKER_STATE}... no commands found."
