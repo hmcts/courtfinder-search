218c53cef38aa1e18bcf8d3fb6c3f2a91026e397
Karol Grecki <karol.grecki@solirius.com>
2019-09-09 10:12:46 +0100

    fix tests

--- courtfinder/search/tests/test_search.py
+++ courtfinder/search/tests/test_search.py
@@ -71,24 +71,10 @@ class SearchTestCase(TestCase):
 
     def test_spoe_page_with_has_spoe(self):
         c = Client()
-        response = c.get('/search/spoe?aol=Children')
+        response = c.get('/search/spoe?aol=Divorce')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'search/spoe.jinja')
-        self.assertContains(response, '<h1>Children</h1>')
-
-    def test_spoe_page_with_children_start_proceedings(self):
-        c = Client()
-        response = c.get('/search/spoe?aol=Children')
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'search/spoe.jinja')
-        self.assertContains(response, 'I want to start new proceedings')
-
-    def test_spoe_page_with_children_in_contact(self):
-        c = Client()
-        response = c.get('/search/spoe?aol=Children')
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'search/spoe.jinja')
-        self.assertContains(response, 'I am already in contact')
+        self.assertContains(response, '<h1>Divorce</h1>')
 
     def test_spoe_page_with_children_in_contact_search_courts(self):
         c = Client()
