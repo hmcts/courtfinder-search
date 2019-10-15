c53b9f6a4c85f48fa207dd0265a3c8bdababe698
Karol Grecki <karol.grecki@solirius.com>
2019-10-15 15:53:51 +0100

    Fix azure secrets lookup

--- courtfinder/courtfinder/settings/__init__.py
+++ courtfinder/courtfinder/settings/__init__.py
@@ -6,8 +6,15 @@ SECRETS_PATH = os.environ.get('SECRETS_PATH', os.path.expanduser('~/kvmnt/ctf/')
 
 def secrets(key, default=None):
     """ Retrieve variables from mounted azure key value store """
+    azure_secret = os.getenv(key)
+    if not azure_secret:
+        return default
+
+    # <azure-secret:env-cf-key>
+    file_name = azure_secret[14:-1]
+
     try:
-        with open(SECRETS_PATH + key, 'r') as file:
+        with open(SECRETS_PATH + file_name, 'r') as file:
             value = file.read().strip()
         return value
     except FileNotFoundError as e:
