16ea1ae507093779eede0c4bb21b346ceeb6e9d4
Karol Grecki <karol.grecki@solirius.com>
2019-11-14 12:16:00 +0000

    Don't rate limit logins in test

--- courtfinder/admin/auth.py
+++ courtfinder/admin/auth.py
@@ -1,9 +1,11 @@
 from ratelimit.decorators import ratelimit
 from django.contrib.auth.views import LoginView
 from django.shortcuts import render
+from django.conf import settings
+
 
 class ThrottledLoginView(LoginView):
-    @ratelimit(key='post:username', rate='10/h', method=['POST'], block=True)
+    @ratelimit(key='post:username', rate='10/h', method=['POST'], block=settings.RATELIMIT_LOGIN)
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
 
